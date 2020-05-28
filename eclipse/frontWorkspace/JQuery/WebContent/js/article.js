f039e01b2149fb07c3c48666f8c28a5c81406e24
ednadev <ednadev@naver.com>
2020-05-28 17:47:20 +0900

    Front UI - jQuery : animate, form

---
+++ eclipse/frontWorkspace/JQuery/WebContent/js/article.js
@@ -0,0 +1,43 @@
+$(function() {
+    //1. p태그가 몇개인지 alert창으로 확인...
+    //alert($('p').length);
+
+    //2. 2번째 p태그를 firstP라는 변수에 할당
+    var firstP = $('p:eq(1)');
+    //alert(firstP.html()); //text()
+
+    $('a.more').click(function() {
+        if(firstP.is(':hidden')){ //안보인다면
+            firstP.slideToggle('slow');
+            $(this).html('read less');
+        }else { //보인다면
+            firstP.slideToggle('slow');
+            $(this).html('read more');
+        }
+    });//click
+
+    //4.
+    var speech = $('div.speech');
+    var defaultSize = speech.css('fontSize');
+    //alert(defaultSize); //19.2px
+
+    var num = parseInt(defaultSize, 10);
+    //alert(num);
+
+    $('#switcher>button').click(function() {
+        switch(this.id) {
+            case 'switcher-large':
+                num += 5;
+                break;
+            case 'switcher-small':
+                num -= 5;
+                break;
+            default:
+                num = parseInt(defaultSize, 10);
+        }//switch
+
+        //반드시 animate() 적용해줘야 한다...
+        speech.animate({fontSize: num + 'px'}, 'slow');
+
+    });
+});
